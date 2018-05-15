2311e213728b888ede1652664c7725e7a31f727a
nekrotzar <peixeocavalovoador@hotmail.com>
2018-05-15 17:40:52 +0100

    Add main menu

---
+++ P3DGame/Assets/script/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Start a new game
+	public void StartGame()
+	{
+		// Load game scene
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+
+	// Quit game
+	public void QuitGame()
+	{
+		Application.Quit ();
+	}
+}
