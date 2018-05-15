72b51d93516f0377a7a5bf7cedd532ceaae96655
Ricardo Fonseca <ric.bicas@gmail.com>
2018-05-15 18:25:27 +0100

    Adds crosshair for main scene

--- P3DGame/Assets/script/MainMenu.cs
+++ P3DGame/Assets/script/MainMenu.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour {
-
-	// Start a new game
+public class MainMenu : MonoBehaviour
+{
+    // Start a new game
 	public void StartGame()
 	{
 		// Load game scene
