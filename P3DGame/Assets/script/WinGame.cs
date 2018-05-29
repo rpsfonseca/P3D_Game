65188964e6bda600601472644196efdea2e231a0
nekrotzar <peixeocavalovoador@hotmail.com>
2018-05-29 17:08:18 +0100

    Add lightmap and bump map texture

---
+++ P3DGame/Assets/script/WinGame.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinGame : MonoBehaviour {
+
+
+	public Text victoryText;
+	public GameObject hud;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.name == "Player") 
+		{
+			Instantiate<Text> (victoryText, hud.transform);
+			StartCoroutine (waiter ());
+		}
+	}
+
+	IEnumerator waiter()
+	{
+		yield return new WaitForSeconds(5);
+		GameManager.instance.KillPlayer ();
+	}
+
+
+}
